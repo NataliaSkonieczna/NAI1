a484cdd8152ae253bcfcc0c03f62a8fe95b578ba
NataliaSkonieczna <natalia.skonieczna2001@wp.pl>
2022-10-07 16:46:59 +0200

    hmwrk

---
+++ cmake-build-debug/CMakeFiles/NAI1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NAI1.dir/zad1.cpp.obj"
+  "CMakeFiles/NAI1.dir/zad1.cpp.obj.d"
+  "NAI1.exe"
+  "NAI1.exe.manifest"
+  "NAI1.pdb"
+  "libNAI1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NAI1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- zad1.cpp
+++ zad1.cpp
@@ -7,29 +7,42 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <cmath>
 
-using my_fun = std::function<int(int)>;
-using my_map = std::map<int, int>;
+using my_fun = std::function<double(std::vector<double>)>;
 
-void lab1(my_map map, my_fun){
+void lab1(std::vector<double> num, my_fun fun){
     using namespace std;
-
-        cout << "" << endl;
-
+        cout << fun(num) << endl;
 }
 
 int main(int argc, char **argv) {
     using namespace std;
     map<int, int> map1 = {{}};
     map<string, my_fun> format;
-    format["add"] = [](int x) { return x; };
-    format["mod"] = [](int x) { return x; };
-    format["sin"] = [](int x) { return x; };
+    format["add"] = [](vector<double> num) { return num.front() + num.back(); };
+    format["mod"] = [](vector<double> num) { return (int)num.front() % (int)num.back(); };
+    format["sin"] = [](vector<double> num) { return sin(num.front()); };
+
 
     try {
-        vector<string> argumenty(argv, argv + argc);
-        auto selected_f = argumenty.at(1);
-        lab1(map1, format.at(selected_f));
+        vector<string> argument(argv, argv + argc);
+        auto selected_f = argument.at(2);
+        if(argument.size() > 5){
+            cout << "za wiele argumentow" <<endl;
+            return 1;
+        }
+        else if(selected_f == "sin" && argument.size() > 4){
+            cout << "za wiele argumentow" <<endl;
+            return 1;
+        }
+        else if(argument.at(1)!="lab1"){
+            cout << "na poczatku powinno pojawic sie 'lab1'" <<endl;
+            return 1;
+        }
+
+        vector<double> num = {{stod(argument.at(3)), stod(argument.back())}};
+        lab1(num, format.at(selected_f));
     } catch (std::out_of_range aor) {
         cout << "podaj argument. Dostepne to: ";
         for (auto [k, v] : format) cout <<endl << " " << k;
