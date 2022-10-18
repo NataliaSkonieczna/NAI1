2a40b1cf16f578b743f88f79d5f4bb6fdf6690d9
NataliaSkonieczna <natalia.skonieczna2001@wp.pl>
2022-10-18 17:05:55 +0200

    trying

--- zad2.cpp
+++ zad2.cpp
@@ -23,31 +23,26 @@ auto brute_force = [](auto f, auto domain) {
 };
 
 
-
 using domain_t = std::vector<double>;
 std::random_device rd;
 std::mt19937 mt_generator(rd());
 
-domain_t hill_climbing(const std::function<double(domain_t)> &f, domain_t minimal_d, domain_t maximal_d, int max_iterations) {
-    domain_t current_p(minimal_d.size());
+double hill_climbing(std::function<double(double, double)> &f,domain_t minimal_d, domain_t maximal_d, int max_iterations=1000) {
     for (int i = 0; i < minimal_d.size(); i++) {
         std::uniform_real_distribution<double> dist(minimal_d[i], maximal_d[i]);
-        current_p[i] = dist(mt_generator);
-    }
-    for (int iteration = 0; iteration < max_iterations; iteration++) {
-        domain_t new_p(minimal_d.size());
-        for (int i = 0; i < minimal_d.size(); i++) {
-            std::uniform_real_distribution<double> dist(-1.0/128.0, 1.0/128.0);
-            new_p[i] = current_p[i] + dist(mt_generator);
+        double best_p = f(minimal_d[i],maximal_d[i]);
+
+        for(int j=0; j<max_iterations-1 ;j++){
+
+            double current_p = dist(mt_generator);
+
+            if(current_p < current_p){
+                current_p = best_p;
+            }
         }
-        if (f(current_p) > f(new_p)) {
-            current_p = new_p;
-        }
     }
-    return current_p;
+    return 0;
 }
-
-
 
 int main() {
 
@@ -61,20 +56,19 @@ int main() {
     auto best_point = brute_force(sphere_f, sphere_generator);
     std::cout << "best x = " << best_point << std::endl;
 
-    auto beale = [](domain_t x) {return sqrt(1.5-x[0]+x[0]*x[1]) + sqrt(2.25-x[0]+sqrt(x[0]*x[1]))+sqrt(2.625-x[0]+cbrt(x[0]*x[1]));};
-    auto best2 = hill_climbing(beale, {-4.5},{4.5},10000);
-    std::cout << "best x = " << best2[0] << std::endl;
+
+    std::function<double(double,double)> beale = [](double x, double y){return pow(1.5-x+x*y,2) + pow(2.25-x+sqrt(x*y),2)+pow(2.625-x+std::pow(x*y, 1/3.),2);};
+    std::cout << "best x = " << hill_climbing(beale,{-4.5},{4.5},10000) << std::endl;
+
+    std::function<double(double,double)> booth = [](double x, double y){return pow(x+2*y-7,2)+pow(2*x+y-5,2);};
+    std::cout << "best x = " << hill_climbing(booth,{-4.5},{4.5},10000) << std::endl;
+
+    std::function<double(double,double)> matyas = [](double x, double y){return 0.26*((x*x)+(y*y))-0.48*x*y;};
+    std::cout << "best x = " << hill_climbing(matyas,{-4.5},{4.5},10000) << std::endl;
 
 
-    auto booth = [](domain_t x) {return sqrt(x[0]+2*x[0]-7)+ sqrt(2*x[0]+x[1]-5);};
-    auto best3 = hill_climbing(beale, {-10},{10},10000);
-    std::cout << "best x = " << best3[0] << std::endl;
-
-
-    auto matyas = [](domain_t x) {return 0.26*(sqrt(x[0])+ sqrt(x[1])-0.48*x[0]*x[1]);};
-    auto best4 = hill_climbing(matyas, {-10},{10},10000);
-    std::cout << "best x = " << best4[0] << std::endl;
 
 
     return 0;
 }
+
