ee8c48dfd1e5599668c43abd7dba127274aeee9f
NataliaSkonieczna <76210735+NataliaSkonieczna@users.noreply.github.com>
2022-10-19 16:04:46 +0200

    quickfix

--- zad2.cpp
+++ zad2.cpp
@@ -3,6 +3,7 @@
 #include <functional>
 #include <random>
 #include <chrono>
+#include <fstream>
 /**
  * domain - generate domain points. Throws exception when all the points were returned
  */
@@ -28,6 +29,8 @@ auto brute_force = [](auto f, auto domain) {
 using domain_t = std::vector<double>;
 std::random_device rd;
 std::mt19937 mt_generator(rd());
+std::ofstream outfile("file.txt");
+
 
 double hill_climbing(std::function<double(double, double)> &f,domain_t minimal_d, domain_t maximal_d, int max_iterations=1000) {
 
@@ -36,11 +39,14 @@ double hill_climbing(std::function<double(double, double)> &f,domain_t minimal_d
     using std::chrono::duration;
     using std::chrono::milliseconds;
 
+    int times= max_iterations/20;
+
     auto t1 = high_resolution_clock::now();
     std::uniform_real_distribution<double> dist(minimal_d[0], maximal_d[0]);
     double current_p = f(minimal_d[0], maximal_d[0]);
     double new_p(minimal_d.size());
     double new_p2(minimal_d.size());
+
 
     for (int iteration = 0; iteration < max_iterations; iteration++) {
 
@@ -50,11 +56,22 @@ double hill_climbing(std::function<double(double, double)> &f,domain_t minimal_d
         if (f(new_p,new_p2) < current_p){
             current_p = f(new_p,new_p2);
         }
+
+        if(iteration % times == 0){
+            auto t2 = high_resolution_clock::now();
+            auto ms_int = duration_cast<milliseconds>(t2 - t1);
+            duration<double, std::milli> ms_double = t2 - t1;
+            std::cout << std::endl << "time = " << ms_double.count() << "ms\n";
+
+            outfile.open("file.txt",std::ios_base::app);
+            outfile<<ms_double.count()<< " " << current_p << std::endl;
+            outfile.close();
+        }
+
+
     }
-    auto t2 = high_resolution_clock::now();
-    auto ms_int = duration_cast<milliseconds>(t2 - t1);
-    duration<double, std::milli> ms_double = t2 - t1;
-    std::cout << std::endl << "time = " << ms_double.count() << "ms\n";
+
+
     std::cout << "best x = ";
     return current_p;
 }
@@ -62,10 +79,13 @@ double hill_climbing(std::function<double(double, double)> &f,domain_t minimal_d
 
 int main() {
 
+    outfile.open("file.txt",std::ios_base::app);
+    outfile<<std::endl;
+    outfile.close();
 
     std::function<double(double,double)> beale = [](double x, double y){return pow(1.5 - x + (x * y), 2) + pow(2.25 - x + x * pow(y,2), 2) + pow(2.625 - x + (x * pow(y, 3)), 2);};
-
     std::cout << hill_climbing(beale,{-4.5},{4.5},10000) << std::endl;
+
 
     std::function<double(double,double)> booth = [](double x, double y){return pow(x+2*y-7,2)+pow(2*x+y-5,2);};
     std::cout << hill_climbing(booth,{-4.5},{4.5},10000) << std::endl;
