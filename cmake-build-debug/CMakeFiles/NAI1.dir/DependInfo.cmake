ce4ddce76b31f85b1a7f938e27241740ec849018
NataliaSkonieczna <76210735+NataliaSkonieczna@users.noreply.github.com>
2022-10-26 15:23:21 +0200

    better md

--- cmake-build-debug/CMakeFiles/NAI1.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/NAI1.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "E:/github/NAI1/main.cpp" "CMakeFiles/NAI1.dir/main.cpp.obj" "gcc" "CMakeFiles/NAI1.dir/main.cpp.obj.d"
+  "C:/Users/s22785/Documents/GitHub/NAI1/main.cpp" "CMakeFiles/NAI1.dir/main.cpp.obj" "gcc" "CMakeFiles/NAI1.dir/main.cpp.obj.d"
   )
 
 # Targets to which this target links.
